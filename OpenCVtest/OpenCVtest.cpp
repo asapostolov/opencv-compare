cbfd7f3c7ff9c1eb3bbecd48890e5aea2d6ea0c3
Apostol Apostolov <apostolov87@gmail.com>
2013-08-14 23:32:16 +0300

    testing with histograms

--- OpenCVtest/OpenCVtest.cpp
+++ OpenCVtest/OpenCVtest.cpp
@@ -71,8 +71,22 @@ int main( int argc, char** argv )
 
   calcHist( &hsv_test2, 1, channels, Mat(), hist_test2, 2, histSize, ranges, true, false );
   normalize( hist_test2, hist_test2, 0, 1, NORM_MINMAX, -1, Mat() );
+  
+  
+  cv::Mat my_histogram;
+  cv::FileStorage fs("C:\\GitHub\\opencv-compare\\samples\\my_histogram_file.yml", cv::FileStorage::WRITE);
+	if (!fs.isOpened()) 
+	{
+		std::cout << "unable to open file storage!" << std::endl; 
+		//return;
+	}
+	fs << "my_histogram" << hist_base;
+	fs.release();
+  
+	cv::FileStorage fs2("test.yml", FileStorage::READ);
 
-  /// Apply the histogram comparison methods
+
+	/// Apply the histogram comparison methods
   for( int i = 0; i < 4; i++ )
      { int compare_method = i;
        double base_base = compareHist( hist_base, hist_base, compare_method );
@@ -88,4 +102,67 @@ int main( int argc, char** argv )
   system ("pause");
        
   return 0;
+
+ // 
+ //  Mat src, hsv;
+ //   if(!(src=imread(argv[1], 1)).data )
+ //       return -1;
+
+ //   cvtColor(src, hsv, CV_BGR2HSV);
+
+ //   // Quantize the hue to 30 levels
+ //   // and the saturation to 32 levels
+ //   int hbins = 150, sbins = 150;
+ //   int histSize[] = {hbins, sbins};
+ //   // hue varies from 0 to 179, see cvtColor
+ //   float hranges[] = { 0, 180 };
+ //   // saturation varies from 0 (black-gray-white) to
+ //   // 255 (pure spectrum color)
+ //   float sranges[] = { 0, 256 };
+ //   const float* ranges[] = { hranges, sranges };
+ //   MatND hist;
+ //   // we compute the histogram from the 0-th and 1-st channels
+ //   int channels[] = {0, 1};
+
+ //   calcHist( &hsv, 1, channels, Mat(), // do not use mask
+ //            hist, 2, histSize, ranges,
+ //            true, // the histogram is uniform
+ //            false );
+ //   double maxVal=0;
+ //   minMaxLoc(hist, 0, &maxVal, 0, 0);
+
+ //   int scale = 5;
+ //   Mat histImg = Mat::zeros(sbins*scale, hbins*10, CV_8UC3);
+
+ //   for( int h = 0; h < hbins; h++ )
+ //       for( int s = 0; s < sbins; s++ )
+ //       {
+ //           float binVal = hist.at<float>(h, s);
+ //           int intensity = cvRound(binVal*255/maxVal);
+ //           rectangle( histImg, Point(h*scale, s*scale),
+ //                       Point( (h+1)*scale - 1, (s+1)*scale - 1),
+ //                       Scalar::all(intensity),
+ //                       CV_FILLED );
+ //       }
+
+ //   namedWindow( "Source", 1 );
+ //   imshow( "Source", src );
+
+ //   namedWindow( "H-S Histogram", 1 );
+ //   imshow( "H-S Histogram", histImg );
+
+	//cv::Mat my_histogram;
+ //   cv::FileStorage fs("C:\\GitHub\\opencv-compare\\samples\\my_histogram_file.yml", cv::FileStorage::WRITE);
+	//if (!fs.isOpened()) 
+	//{
+	//	std::cout << "unable to open file storage!" << std::endl; 
+	//	//return;
+	//}
+	//fs << "my_histogram" << hist;
+	//fs.release();
+ // 
+	//cv::FileStorage fs2("test.yml", FileStorage::READ);
+
+
+ //   waitKey();
  }
